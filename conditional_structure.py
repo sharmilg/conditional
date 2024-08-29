75a10a97d0095ebffb580f511518a8d6e7e26356
sharmilg <sharmilg@metropolia.fi>
2024-08-29 12:44:45 +0300

    task 3

--- conditional_structure.py
+++ conditional_structure.py
@@ -2,7 +2,7 @@
 # If the zander does not fulfill the size limit, the program instructs to release the fish back
 # into the lake and notifies the user of how many centimeters below the size limit the caught fish was.
 # A zander must be 42 centimeters or longer to meet the size limit.
-
+'''
 length = float(input("Enter the length of the zander in centimeters: "))
 size_limit = 42
 if length >= size_limit:
@@ -33,20 +33,42 @@ elif cabin_class == "C":
 else:
     print("Invalid cabin class")
 
-
 #Write a program that asks the user to enter a year and notifies the user whether the input year is a leap year.
 # A year is a leap year if it is divisible by four.
 # However, years divisible by 100 are leap years only if they are also divisible by 400.
 
-
-
-
-
+year = ""
+while year !="exit" :
+    year = input("Enter the year or type 'exit' to terminate the program.: ")
+    if year == "exit":
+        break
+    year =int(year)
+    if (year%100==0 and year%400==0) or (year%4==0):
+            print(f" The year {year} is a leap year ")
+    else:
+            print(f" The year {year} is not a leap year ")
+'''
 
 #Write a program that asks for the biological gender and hemoglobin value (g/l).
 # The program the notifies the user if the hemoglobin value is low, normal or high.
 #A normal hemoglobin value for adult females is between 117-155 g/l.
 #A normal hemoglobin value for adult males is between 134-167 g/l.
+
+gender = input("Enter your biological gender ( male/female): ")
+hemoglobin = int(input("Enter your hemoglobin (hemoglobin): "))
+if gender == "male" and 134 <=hemoglobin<=167:
+    print(" Your hemoglobin level is normal")
+else:
+    if gender == "male" and hemoglobin<134:
+        print(" Your hemoglobin level low")
+    elif gender == "male" and hemoglobin>167:
+        print(" Your hemoglobin level high")
+    if gender == "female" and 117<= hemoglobin<=155:
+        print(" Your hemoglobin level high")
+    elif gender == "female" and hemoglobin<117:
+        print(" Your hemoglobin level low")
+    elif gender == "female" and hemoglobin>155:
+        print(" Your hemoglobin level high")
 
 
 
