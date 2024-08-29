086adcd805898a9ee17a49935ba50ed88106639d
sharmilg <sharmilg@metropolia.fi>
2024-08-29 12:51:46 +0300

    task 3

--- conditional_structure.py
+++ conditional_structure.py
@@ -6,7 +6,7 @@
 length = float(input("Enter the length of the zander in centimeters: "))
 size_limit = 42
 if length >= size_limit:
-    print("The zander meets the size limit:")
+    print("The zander meets the size limit.")
 else:
     difference = size_limit-length
     print(f" The zander is {difference:.2f} centimeter below the size limit.")
