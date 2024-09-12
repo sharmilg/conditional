c71ed75acf2d5947b6993f5751444533668b12cf
sharmilg <sharmilg@metropolia.fi>
2024-09-12 15:31:02 +0300

    task 6

--- task6.py
+++ task6.py
@@ -91,33 +91,32 @@ print(f"List without odd numbers:{list}")
 
 import math
 
+def calculate_unit_price(diameter, price):
 
-def calculate_unit_price(diameter_cm, price_euros):
-
-    radius_m = (diameter_cm / 2) / 100
-    area_sqm = math.pi * (radius_m ** 2)
-    unit_price = price_euros / area_sqm
+    radius = (diameter / 2) / 100
+    area = math.pi * (radius ** 2)
+    unit_price = price / area
     return unit_price
 
 
-def main():
-    diameter1 = float(input("Enter the diameter of the first pizza (cm): "))
-    price1 = float(input("Enter the price of the first pizza (euros): "))
 
-    diameter2 = float(input("Enter the diameter of the second pizza (cm): "))
-    price2 = float(input("Enter the price of the second pizza (euros): "))
-    unit_price1 = calculate_unit_price(diameter1, price1)
-    unit_price2 = calculate_unit_price(diameter2, price2)
-    print(f"Unit price of the first pizza: {unit_price1:.2f} euros per square meter")
-    print(f"Unit price of the second pizza: {unit_price2:.2f} euros per square meter")
-    if unit_price1 < unit_price2:
-        print("The first pizza provides better value for money.")
-    elif unit_price1 > unit_price2:
-        print("The second pizza provides better value for money.")
-    else:
-        print("Both pizzas provide the same value for money.")
 
-if __name__ == "__main__":
-    main()
+
+diameter1 = float(input("Enter the diameter of the first pizza (cm): "))
+price1 = float(input("Enter the price of the first pizza (euros): "))
+
+diameter2 = float(input("Enter the diameter of the second pizza (cm): "))
+price2 = float(input("Enter the price of the second pizza (euros): "))
+unit_price1 = calculate_unit_price(diameter1, price1)
+unit_price2 = calculate_unit_price(diameter2, price2)
+print(f"Unit price of the first pizza: {unit_price1:.2f} euros per square meter")
+print(f"Unit price of the second pizza: {unit_price2:.2f} euros per square meter")
+if unit_price1 < unit_price2:
+    print("The first pizza provides better value for money.")
+elif unit_price1 > unit_price2:
+    print("The second pizza provides better value for money.")
+else:
+    print("Both pizzas provide the same value for money.")
+
 
 
